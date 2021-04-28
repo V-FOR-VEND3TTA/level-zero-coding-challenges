91942f07e55a547b2d2c36cebe40307c48c16df1
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-04-28 17:23:06 +0200

    Removed redundant comments

--- Task 0.5/script.js
+++ Task 0.5/script.js
@@ -1,9 +1,5 @@
-// Finding the area of a triangle with 3 sides
-// Used Heron's formula of 3 side lengths to get the area
 function areaOfTriangle(sideOne, sideTwo, sideThree) {
-  // Finding the semi-perimeter
   let sp = (sideOne + sideTwo + sideThree) / 2;
-  // Calculating the area
   let area = Math.sqrt(
     sp * ((sp - sideOne) * (sp - sideTwo) * (sp - sideThree))
   );
