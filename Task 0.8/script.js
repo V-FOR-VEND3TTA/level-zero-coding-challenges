cfe2bc0a6131bfeec54423262222792f9306be81
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-04-28 17:42:02 +0200

    Adapted to handle both singulars and plurals

--- Task 0.8/script.js
+++ Task 0.8/script.js
@@ -1,7 +1,11 @@
 function numberToTime(num) {
   var hours = Math.floor(num / 60);
   var minutes = num % 60;
-  return hours + " hour(s)," + minutes + " minute(s)";
+  if (if hours === 60 && minutes === 1) {
+    return hours + " hour," + minutes + " minutes";
+  } else {
+  return hours + " hours," + minutes + " minutes";
+  }
 }
 
 console.log(numberToTime(400));
