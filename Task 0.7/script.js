d67a4e2e23b99de0bfbc0e877f92d58fda743f39
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-04-28 17:29:26 +0200

    Removed redundant comments

--- Task 0.7/script.js
+++ Task 0.7/script.js
@@ -1,11 +1,6 @@
-// Formula to convert celsius to fahrenheit: fahrenheit = celsius * 1.8 + 32
 function degreesToFahrenheit(temp) {
   const fahrenheit = temp * 1.8 + 32;
-  /* Using concatenation
-  console.log(
-    temp + " degrees celsius is equal to " + fahrenheit + " degrees fahrenheit."
-  );*/
-  // Using ES6 Syntax
+  
   console.log(
     `${temp} degrees celsius is equal to ${fahrenheit} degree fahrenheit.`
   );
@@ -13,7 +8,6 @@ function degreesToFahrenheit(temp) {
 
 degreesToFahrenheit(35);
 
-// Formula to convert fahrenheit to celsius: celsius = temp
 function fahrenheitToDegrees(temp) {
   const celsius = (temp - 32) / 1.8;
   console.log(
