7430fffdd49c8136562c428d7ef02b7ff446685c
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-04-28 17:24:44 +0200

    Removed redundant else if statement

--- Task 0.6/script.js
+++ Task 0.6/script.js
@@ -4,7 +4,7 @@ function findTheMax(num1, num2, num3) {
     console.log(`${num1} is the largest`);
   } else if (num2 > num1 && num2 > num3) {
     console.log(`${num2} is the largest`);
-  } else if (num3 > num1 && num3 > num2) {
+  } else {
     console.log(`${num3} is the largest`);
   }
 }
